3231f81667e79a511b794cceee65b5f6d9d38c32
Soumajit Chakraborty <156880318+soumajit67@users.noreply.github.com>
2026-09-21 09:05:31 +0530

    Enhance timestamp formatting in ApiError class
    Updated timestamp formatting to include time zone and detailed date components.

--- ApiError.js
+++ ApiError.js
@@ -7,7 +7,30 @@ class ApiError extends Error {
     this.statusCode = statusCode;
     this.message = message;
     this.errors = errors;
-    this.timestamp = new Date().toLocaleString('en-IN');
+    this.timestamp = new Date().toLocaleString('en-IN'class ApiError extends Error {
+  constructor(statusCode, message = "Something went wrong", errors = []) {
+    super(message);
+
+    this.success = false;
+    this.statusCode = statusCode;
+    this.message = message;
+    this.errors = errors;
+    this.timestamp = new Date().toLocaleString('en-IN', {
+    timeZone: 'Asia/Kolkata',
+    day: '2-digit',
+    month: '2-digit',
+    year: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric',
+    hour12: true 
+  });
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
+
+export default ApiError;
+);
     Error.captureStackTrace(this, this.constructor);
   }
 }
