aba5a49482d7bcf5199a68b63a1fe2414b523baa
Soumajit Chakraborty <156880318+soumajit67@users.noreply.github.com>
2026-09-22 08:52:54 +0530

    Refactor timestamp initialization in ApiResponse
    Updated timestamp initialization to use a specific locale and timezone.

--- ApiResponse.js
+++ ApiResponse.js
@@ -6,13 +6,6 @@ class ApiResponse {
     this.message = message;
     this.data = data;
 
-    this.timestamp = new Date().toLocaleString('en-IN'class ApiResponse {
-  constructor(statusCode, message = "Success", data = null) {
-    this.success = statusCode < 400;
-    this.statusCode = statusCode;
-    this.message = message;
-    this.data = data;
-
     this.timestamp = new Date().toLocaleString('en-IN', {
     timeZone: 'Asia/Kolkata',
     day: '2-digit',
