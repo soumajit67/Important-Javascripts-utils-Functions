523be0737540e1208d6a461c39ff69e776457c2c
Soumajit Chakraborty <156880318+soumajit67@users.noreply.github.com>
2026-09-22 08:52:17 +0530

    Update ApiResponse.js

--- ApiResponse.js
+++ ApiResponse.js
@@ -6,7 +6,28 @@ class ApiResponse {
     this.message = message;
     this.data = data;
 
-    this.timestamp = new Date().toLocaleString('en-IN');
+    this.timestamp = new Date().toLocaleString('en-IN'class ApiResponse {
+  constructor(statusCode, message = "Success", data = null) {
+    this.success = statusCode < 400;
+    this.statusCode = statusCode;
+    this.message = message;
+    this.data = data;
+
+    this.timestamp = new Date().toLocaleString('en-IN', {
+    timeZone: 'Asia/Kolkata',
+    day: '2-digit',
+    month: '2-digit',
+    year: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric',
+    hour12: true 
+  });
+  }
+}
+
+export default ApiResponse;
+);
   }
 }
 
